651363b83757c408716abff6fcd35ad07ae842df
shehanrangana <shehanrnet@gmail.com>
2018-09-26 00:22:29 +0530

    migrations created

---
+++ database/migrations/2018_09_25_180838_create_drugs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDrugsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drugs', function (Blueprint $table) {
+            $table->increments('drug_id');
+            $table->string('name', 50)->index();
+            $table->string('brand', 30);
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->string('expire_date', 10);
+            $table->string('supplier_email', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drugs');
+    }
+}

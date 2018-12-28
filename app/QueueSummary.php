eb5a03d367a052388b46c1c5fbc9d5be783bd2d3
shehanrangana <shehanrnet@gmail.com>
2018-12-28 23:15:37 +0530

    Added:Doctor can retrieve current queue patient list

---
+++ app/QueueSummary.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QueueSummary extends Model
+{
+    protected $table = "queue_summary";
+}
--- resources/js/components/Doctor/Dashboard.vue
+++ resources/js/components/Doctor/Dashboard.vue
@@ -1,9 +1,35 @@
 <template>
     <div class="inner-div">
-        <h3>Doctor Dashboard</h3>
+        <!-- Queue list -->
+        <b-list-group>
+            <b-list-group-item  v-for="patient in patientList" :key="patient.index" href="#some-link">{{ patient.number+ " " +patient.patient_id + " " +patient.name}}</b-list-group-item>
+        </b-list-group>
     </div>
 </template>
 
 <script>
-    
+import { ListGroup } from 'bootstrap-vue/es/components';
+Vue.use(ListGroup);
+
+export default {
+    data () {
+        return {
+            patientList: [],
+        }
+    },
+
+    mounted() {
+        console.log("App mounted");
+        this.getQueueList();
+    },
+
+    methods: {
+        getQueueList() {
+            axios.get('/doctor/dashboard/get_queue').then( (response)=>{
+                this.patientList = response.data;
+                // console.log(response.data);
+            })
+        },
+    }
+}
 </script>
